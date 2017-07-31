c5310c771a2a595052a50302a21527f7c64654ab
zwan <zhentian.wan@ssh.com>
2017-07-31 16:43:55 +0300

    broswer polyfills support

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+if (!window.Intl) {
+  require.ensure([
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+    'intl/locale-data/jsonp/fr.js',
+    'intl/locale-data/jsonp/es.js'
+  ], (require) => {
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    require('intl/locale-data/jsonp/fr.js');
+    require('intl/locale-data/jsonp/es.js');
 
-import { addLocaleData, IntlProvider } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import fr from 'react-intl/locale-data/fr';
-import es from 'react-intl/locale-data/es';
+    runApp();
+  })
+} else {
+  runApp();
+}
 
-import messages from './messages';
-import { flattenMessages } from './utils'; 
+function runApp() {
+  addLocaleData([...en, ...fr, ...es]);
 
-import App from './App';
-import './index.css';
+  let locale = (navigator.languages && navigator.languages[0])
+    || navigator.language
+    || navigator.userLanguage
+    || 'en-US';
 
-addLocaleData([...en, ...fr, ...es]);
-
-let locale = (navigator.languages && navigator.languages[0])
-             || navigator.language
-             || navigator.userLanguage
-             || 'en-US';
-
-ReactDOM.render(
-  <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
-    <App />
-  </IntlProvider>,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+    <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+      <App />
+    </IntlProvider>,
+    document.getElementById('root')
+  );
+}
